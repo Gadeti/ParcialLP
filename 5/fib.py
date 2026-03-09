6de891e575c339c039bbfa9b94454a39a857bff4
Gadeti <gadeti23@gmail.com>
2026-03-09 15:42:00 -0500

    Add Fibonacci function and user input handling
    Implement Fibonacci sequence generator with user input.

--- 5/fib.py
+++ 5/fib.py
@@ -1,1 +1,34 @@
+FIBO(8)
 
+"""
+
+def fibonacci(n):
+
+    a = 0
+    b = 1
+
+    secuencia = []
+
+    for i in range(n):
+        secuencia.append(a)
+        a, b = b, a + b
+
+    return secuencia
+
+
+entrada = input("Ingrese numero: ")
+
+n = int(entrada)
+
+print(fibonacci(n))
+
+
+"""
+Generar parser ANTLR:
+
+antlr4 -Dlanguage=Python3 fib.g4
+
+Ejecutar programa:
+
+python3 fib.py
+"""
