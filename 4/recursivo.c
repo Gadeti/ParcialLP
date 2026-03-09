7f4b6e5050f1197312fe5a439af781147a0592bc
Gadeti <gadeti23@gmail.com>
2026-03-09 15:30:47 -0500

    Add recursive Fibonacci function with timing
    Implement Fibonacci function using recursion and measure execution time.

--- 4/recursivo.c
+++ 4/recursivo.c
@@ -1,1 +1,40 @@
+/*
+recursivo.c
+*/
 
+#include <stdio.h>
+#include <time.h>
+
+long fib(int n){
+
+    if(n<=1)
+        return n;
+
+    return fib(n-1) + fib(n-2);
+}
+
+int main(){
+
+    clock_t inicio = clock();
+
+    printf("Resultado: %ld\n", fib(35));
+
+    clock_t fin = clock();
+
+    double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
+
+    printf("Tiempo de ejecucion: %f segundos\n", tiempo);
+
+    return 0;
+}
+
+
+/*
+Compilar:
+
+gcc recursivo.c
+
+Ejecutar:
+
+./a.out
+*/
